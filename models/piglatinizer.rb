247ce3e9c985685caae778a323f4defe1980facd
Sharmina Akhtar <sakht318@gmail.com>
2018-03-09 11:13:08 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,19 @@
 class PigLatinizer
 
-  def to_pig_latin(string)
-    string.split.map do |word|
-    if /\A[aeiou]/i.match(word)
-      "#{word}ay"
+  def self.to_pig_latin(word)
+    alpha = ('a'..'z').to_a
+    vowels = %w[a e i o u]
+    consonants = alpha - vowels
+
+    if vowels.include?(str[0])
+       word + 'ay'
+    elsif consonants.include?(str[0]) && consonants.include?(str[1])
+      word[2..-1] + word[0..1] + 'ay'
+    elsif consonants.include?(str[0])
+      word[1..-1] + word[0] + 'ay'
     else
-      "#{word.chars.rotate(1).join}ay"
+      word 
     end
-    end.join(' ')
   end
 
 end
